ccd6d8600e6be3f20ed7eed451b677d2dee50b3d
decky12582-github <63011256+decky12582-github@users.noreply.github.com>
2023-04-27 15:57:36 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,17 @@
+// Set Constants
+const bypass = "https://github.com/Bypass-Central"
+const hostingtxt = "This was created by Bypass Central."
+const gitxt = "Github"
+
+// Function for open video
+function openVideo(nocookievideoLink) {
+var win = window.open()
+var url = "https://www.youtube-nocookie.com/embed/vFNmrGaV6YQ"
+var iframe = win.document.createElement('iframe')
+iframe.style.width = "100%";
+iframe.style.height = "100%";
+iframe.style.border = "none";
+iframe.src = url
+win.document.body.appendChild(iframe)
+win.document.write(hostingtxt)
+}
