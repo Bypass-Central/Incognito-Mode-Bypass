e07157a1accbeaa386db47de6879f4c112f84e7f
decky12582-github <63011256+decky12582-github@users.noreply.github.com>
2023-04-27 16:05:10 -0400

    Update config.js
    switch to parameters

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const gitxt = "Github"
 // Function for open video
 function openVideo(nocookievideoLink) {
 var win = window.open()
-var url = "https://www.youtube-nocookie.com/embed/vFNmrGaV6YQ"
+var url = nocookievideoLink
 var iframe = win.document.createElement('iframe')
 iframe.style.width = "100%";
 iframe.style.height = "100%";
