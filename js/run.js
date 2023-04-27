ac5222ea36ead1ceb81076ff206fe446cd8b31be
decky12582-github <63011256+decky12582-github@users.noreply.github.com>
2023-04-27 15:57:18 -0400

    Update run.js
    seperate js files

--- js/run.js
+++ js/run.js
@@ -1,17 +1,6 @@
-// Set Constants
-const bypass = "https://github.com/Bypass-Central"
-const hostingtxt = "This was created by Bypass Central."
-const gitxt = "Github"
+// import js
+import * as lib from 'config.js'
 
-// Function for open video
-function openVideo(nocookievideoLink) {
-var win = window.open()
-var url = "https://www.youtube-nocookie.com/embed/vFNmrGaV6YQ"
-var iframe = win.document.createElement('iframe')
-iframe.style.width = "100%";
-iframe.style.height = "100%";
-iframe.style.border = "none";
-iframe.src = url
-win.document.body.appendChild(iframe)
-win.document.write(hostingtxt)
-}
+// run function
+
+openGame()
